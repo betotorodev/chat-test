ab97b029eb6f387fa9e45fe8971b53d848fd3c00
Beto Toro <betotoro0902@gmail.com>
2024-09-22 20:02:26 -0500

    feat: create icons

---
+++ icons/search.tsx
@@ -0,0 +1,3 @@
+export const SearchIcon = ({ className }: { className?: string }): JSX.Element => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' className='icon icon-tabler icons-tabler-outline icon-tabler-search'><path d='M0 0h24v24H0z' stroke='none' /><path d='M3 10a7 7 0 1 0 14 0 7 7 0 1 0-14 0M21 21l-6-6' /></svg>
+)
