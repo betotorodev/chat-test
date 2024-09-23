26a9b9c8a33cffd26b04b23caf740f41687e5c07
Beto Toro <betotoro0902@gmail.com>
2024-09-23 10:51:46 -0500

    fix: improve types

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -1,53 +1,54 @@
 'use server'
 import { createClient } from '@/lib/db/supabase/server'
+import { Chat, User } from '@/lib/types'
 
 const supabase = await createClient()
 
-export const createUser = async (user) => {
-  const { data, error } = await supabase
+export const createUser = async (user: User): Promise<void> => {
+  const { error } = await supabase
     .from('users')
     .insert(user)
 
   if (error != null) {
     console.log(error)
-    throw new Error(error)
+    throw new Error(error.message)
   }
 }
 
-export const createChat = async (chat) => {
-  const { data, error } = await supabase
+export const createChat = async (chat: Chat): Promise<void> => {
+  const { error } = await supabase
     .from('chats')
     .insert(chat)
 
   if (error != null) {
     console.log(error)
-    throw new Error(error)
+    throw new Error(error.message)
   }
 }
 
-export const getChats = async (user_id: string) => {
+export const getChats = async (userId: string): Promise<Chat[]> => {
   const { data, error } = await supabase
     .from('chats')
     .select()
-    .eq('user_id', user_id)
+    .eq('user_id', userId)
     .order('created_at', { ascending: false })
 
   if (error != null) {
     console.log(error)
-    throw new Error(error)
+    throw new Error(error.message)
   }
 
   return data
 }
 
-export const updateChat = async (chat) => {
-  const { data, error } = await supabase
+export const updateChat = async (chat: Chat): Promise<void> => {
+  const { error } = await supabase
     .from('chats')
     .update({ list_of_messages: chat.list_of_messages })
     .eq('id', chat.id)
 
   if (error != null) {
     console.log(error)
-    throw new Error(error)
+    throw new Error(error.message)
   }
 }
