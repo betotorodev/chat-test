75ca7a02311fa17b4227c5e7f502815e82d0556f
Beto Toro <betotoro0902@gmail.com>
2024-09-22 19:59:46 -0500

    config: install packages and set configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
-    },
+      keyframes: {
+        dialogOverlayShow: {
+          from: { opacity: '0' },
+          to: { opacity: '1' }
+        },
+        dialogContentShow: {
+          from: {
+            opacity: '0',
+            transform: 'translate(-50%, -45%) scale(0.95)'
+          },
+          to: { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' }
+        }
+      },
+      animation: {
+        // Dialog
+        dialogOverlayShow:
+          'dialogOverlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        dialogContentShow:
+          'dialogContentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
